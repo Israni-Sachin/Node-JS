9e3fe183858fcf86aa5ac419cdb0a2ee1b07a11b
Israni-Sachin <sachinisrani586@gmail.com>
2023-09-04 18:52:31 +0530

    Export and import the modules

---
+++ September/4 Sep/script.js
@@ -0,0 +1,22 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end('Hello World!');
+// }).listen(4800);
+
+const express= require('express');
+const EventEmitter = require('events');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
+
+let sachin=require("./second")
+
+console.log(sachin);
---
+++ September/4 Sep/second.js
@@ -0,0 +1,23 @@
+// let details = {
+//     name: "John",
+//     age: 25,
+//     gender: "male",
+//     city: "New York",
+//     country: "USA",
+//     phone: "0123456789",
+//     address: "123 Main St",
+//     zip: "12345"
+// }
+
+// module.exports = details;
+
+// console.log(exports);
+// console.log(require);
+// console.log(module);
+// console.log(__filename);
+// console.log(__dirname);
+
+const os=require("os");
+
+console.log(os.freemem());
+console.log(os.homedir());
